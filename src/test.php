f2c52930cf72d5be6e291d431fbfc0efc106dec2
jica <jica@users.noreply.github.com>
2015-04-29 11:56:25 +0200

    Update test.php
    paralelizar la resolucion de nombres

--- src/test.php
+++ src/test.php
@@ -2,28 +2,35 @@
 processFile('query.txt');
 
 function processFile($filename){
-	if(is_file($filename)){
-		$file = fopen($filename, "r");
-		readLines($file);
-		fclose($file);
-	}else{
-		echo "$filename 'is not a valid file.";
-	}
+  if(is_file($filename)){
+    $file = fopen($filename, "r");
+    readLines($file);
+    fclose($file);
+  }else{
+    echo "$filename 'is not a valid file.";
+  }
 }
 
 function readLines($file){
-	while(!feof($file)){
-		if($line = fgets($file)){
-			$input = preg_split("/[\s]+/", $line); //Necesaria expr_reg: puede estar separado por varios espacios o varios tabuladores
-			resolv($input[0], $input[1]);
-		}
-	}
+  while(!feof($file)){
+    if($line = fgets($file)){
+      $input = preg_split("/[\s]+/", $line); //Necesaria expr_reg: puede estar separado por varios espacios o varios tabuladores
+      $obj = new Resolutor($input[0], $input[1]);
+      $obj->start();
+    }
+  }
 }
 
-function resolv($hostname, $type){
-	$resolved = dns_get_record($hostname, DNS_PTR);
-	echo "\n----------\n";
-	print_r($resolved);
-}
+class Resolutor extends Thread {
+  public function __construct($hostname, $type){
+    $this->hostname=$hostname;
+    $this->type=$type
+  }
 
+  public function run(){
+    $resolved = dns_get_record($hostname, DNS_PTR);
+    echo "\n----------\n";
+    print_r($resolved);
+  }
+}
 ?>
