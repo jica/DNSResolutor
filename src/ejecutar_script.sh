8e895d130c887a87f939b38a15c57117236a612d
jica <jicenteno@gmail.com>
2015-05-03 21:50:23 +0200

    Scipts bash

--- src/ejecutar_script.sh
+++ src/ejecutar_script.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo 'hola'
+read -p "Introduzca la ruta completa del fichero:" filename
+i=0;
+#for line in $(cat $filename);do
+#  php resolver.php $line
+#  echo $i
+#  i=$i+1
+#done
+while read line
+do
+  ./resolver.sh $line &
+#  php resolver.php $line
+done < $filename
